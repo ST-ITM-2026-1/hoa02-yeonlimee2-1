771828a5f2faafbec2a7a046065bb6e749be9a2e
Seong-bin LEE <yeonlimee@gmail.com>
2026-04-20 11:55:15 +0900

    Add localStorage to keep dark mode

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,18 @@
 const toggle = document.getElementById("theme-toggle");
 
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark");
+  toggle.textContent = "Light";
+}
+
 toggle.addEventListener("click", function() {
   document.body.classList.toggle("dark");
 
   if (document.body.classList.contains("dark")) {
     toggle.textContent = "Light";
+    localStorage.setItem("theme", "dark");
   } else {
     toggle.textContent = "Dark";
+    localStorage.setItem("theme", "light");
   }
 });
